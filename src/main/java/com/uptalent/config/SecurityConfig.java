9a7e3f62a847ad42ffeeb5448d3874f5de88de7a
OLEKSII <convisor19@gmail.com>
2024-04-10 11:56:38 +0300

    implement backend logic

---
+++ src/main/java/com/uptalent/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.uptalent.config;
+
+import com.uptalent.jwt.JwtAuthenticationFilter;
+import com.uptalent.jwt.handler.JwtAccessDeniedHandler;
+import com.uptalent.jwt.handler.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(withDefaults())
+                .csrf().disable()
+                .cors().configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+                    corsConfiguration.addAllowedHeader("*");
+                    corsConfiguration.addExposedHeader("Location");
+                    corsConfiguration.addAllowedMethod(PATCH);
+                    corsConfiguration.addAllowedMethod(PUT);
+                    corsConfiguration.addAllowedMethod(DELETE);
+                    return corsConfiguration;
+                })
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(GET, "/api/v1/talents").permitAll()
+                        .requestMatchers(POST, "/api/v1/talents", "/api/v1/talents/login").permitAll()
+                        .requestMatchers(GET, "/api/v1/proofs").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((exceptionHandling) -> exceptionHandling
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                );
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
